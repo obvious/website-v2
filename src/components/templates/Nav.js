fa6b3fc47dd804a0ccb935ed70592f310a8152c5
Meet Bhatt <meetbhatt@obvious.in>
2019-10-30 19:40:06 +0530

    Remove unused variable

--- src/components/templates/Nav.js
+++ src/components/templates/Nav.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from "styled-components";
 
-import {BodyText2} from "../atoms/BodyText";
 import {HeaderText3} from "../atoms/HeaderText";
 
 const NavContainer = styled.nav`
