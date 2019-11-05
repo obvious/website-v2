22571874570c89add41699d57f506a2587d5e498
Meet Bhatt <meetbhatt@obvious.in>
2019-11-05 11:53:14 +0530

    Remove random logs from homepage

--- src/templates/index.js
+++ src/templates/index.js
@@ -2,13 +2,8 @@ import React from "react"
 import styled from "styled-components";
 
 import {HeaderText1} from '../components/atoms/HeaderText'
-import {HeaderText2} from '../components/atoms/HeaderText'
-import {HeaderText3} from '../components/atoms/HeaderText'
-import {HeaderText4} from '../components/atoms/HeaderText'
-import {HeaderText5} from '../components/atoms/HeaderText'
 
 const IndexPageContainer = styled.div`
-  width: 100vw;
   height: 100vh;
   display: flex;
   align-items: center;
@@ -18,11 +13,7 @@ const IndexPageContainer = styled.div`
 
 const IndexPage = ({pageContext}) => (
     <IndexPageContainer>
-        <HeaderText1>{JSON.stringify(pageContext.data)}</HeaderText1>
-        <HeaderText2>{JSON.stringify(pageContext.data)}</HeaderText2>
-        <HeaderText3>{JSON.stringify(pageContext.data)}</HeaderText3>
-        <HeaderText4>{JSON.stringify(pageContext.data)}</HeaderText4>
-        <HeaderText5>{JSON.stringify(pageContext.data)}</HeaderText5>
+            <HeaderText1>Hello World.</HeaderText1>
     </IndexPageContainer>
 );
 
