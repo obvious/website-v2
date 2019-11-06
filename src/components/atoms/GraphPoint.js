643c7a496112709f74c4a7808c53070dd541fa17
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-11-06 21:34:15 +0530

    Style GraphPoint component

--- src/components/atoms/GraphPoint.js
+++ src/components/atoms/GraphPoint.js
@@ -3,16 +3,25 @@ import styled from "styled-components";
 import {CaptionText2, CaptionTextHighlight} from "./CaptionText";
 import {Divider} from "./Divider";
 
-const GraphPointContainer = styled.div`
-  .caption {
-    max-width: ${props => props.theme.captionWidth};
-  }
+const GraphPointContainer = styled.div`  
+  display: inline-block;
+  max-width: ${props => props.theme.captionWidth};
+  position: relative;
+  left: ${props => Number(props.xCoordinate)}vw;
+  bottom: ${props => Number(props.yCoordinate)}vh;
+  
+  .point {
+    width: ${props => props.theme.spacings[4]};
+    height: ${props => props.theme.spacings[4]};
+    border: ${props => props.theme.colors.gray} solid 2px;
+    border-radius: 50%;
+    background-color: ${props => props.isHighlighted && props.theme.colors.blue};
+    }
 `;
 
 export const GraphPoint = props => (
-         <GraphPointContainer>
+         <GraphPointContainer {...props}>
            <div className="point"></div>
-           <div className="caption">
              <CaptionText2>{props.label[0].text}</CaptionText2>
              {props.specialLabel[0] ? (
                <CaptionTextHighlight>
@@ -20,6 +29,5 @@ export const GraphPoint = props => (
                  {props.specialLabel[0].Text}
                </CaptionTextHighlight>
              ) : null}
-           </div>
          </GraphPointContainer>
        );
