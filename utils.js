857e8f199fca291c96c17d82b70272f529d88b66
Meet Bhatt <meetbhatt@obvious.in>
2019-11-06 18:43:03 +0530

    Modify util function for graphql responses to handle query responses for multiple stories as well

--- utils.js
+++ utils.js
@@ -1,7 +1,21 @@
 const prepeareStoryblokGraphqlResponse = (response) => {
+    let data;
+    if('storyblokEntry' in response) {
+        data = JSON.parse(response.storyblokEntry.content);
+    }
+    else if('allStoryblokEntry' in response) {
+        data = response.allStoryblokEntry.edges.map(edge => {
+            let node = edge.node;
+            node.content = JSON.parse(node.content)
+            return node;
+        });
+    }
+    else {
+        data = {content: `{}` }
+    }
     if(response) {
         try {
-             return JSON.parse(response.storyblokEntry.content);
+             return data;
         } catch(e) {
             throw new Error(e); // error in the above string (in this case, yes)!
         }
