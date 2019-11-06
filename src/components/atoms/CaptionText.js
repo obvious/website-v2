1f149101ed4d30a7634ebfa8d238855395867d6f
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-11-06 17:45:37 +0530

    Rename caption text 1 to be consistent with storyblok component naming

--- src/components/atoms/CaptionText.js
+++ src/components/atoms/CaptionText.js
@@ -1,16 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const CaptionText1Container = styled.figcaption`
+const CaptionText2Container = styled.figcaption`
   font-size: ${props => props.theme.fontSizes[6]};
   line-height: ${props => props.theme.lineHeights[6]};
   font-family: 'SF UI Display Bold';
 `
 
-export const CaptionText1 = ({children, ...otherProps}) => (
-    <CaptionText1Container {...otherProps}>
+export const CaptionText2 = ({children, ...otherProps}) => (
+    <CaptionText2Container {...otherProps}>
         {children}
-    </CaptionText1Container>
+    </CaptionText2Container>
 )
 
 const CaptionTextHighlightContainer = styled.figcaption`
