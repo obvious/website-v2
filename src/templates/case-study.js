e109740b4877d452f880cbd9339a4145c4588658
Meet Bhatt <meetbhatt@obvious.in>
2019-11-06 16:21:41 +0530

    Add space to the bottom of the case study page

--- src/templates/case-study.js
+++ src/templates/case-study.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import styled from "styled-components";
+
 import CaseStudyHeader from "../components/templates/case-studies/CaseStudyHeader";
 import CaseStudySection1 from "../components/templates/case-studies/CaseStudySection1";
 import CaseStudySection2 from "../components/templates/case-studies/CaseStudySection2";
 import CaseStudySection3 from "../components/templates/case-studies/CaseStudySection3";
 import CaseStudyRelatedArticles from "../components/templates/case-studies/CaseStudyRelatedArticles";
 
-const CaseStudy = ({pageContext: {data}}) => {
+const CaseStudyContainer = styled.main`
+  padding-bottom: ${props => props.theme.spacings[1]};
+`;
+
+const CaseStudy = ({pageContext: {data}, ...otherProps}) => {
     return (
-        <div>
+        <CaseStudyContainer {...otherProps}>
             <CaseStudyHeader {...data.header[0]}/>
             <CaseStudySection1 {...data.sections[0]}/>
             <CaseStudySection2 {...data.sections[1]}/>
             <CaseStudySection3 {...data.sections[2]}/>
             <CaseStudyRelatedArticles articles={data.relatedArticles} heading={`Stories from ${data.header[0].title[0].text}`}/>
-        </div>
+        </CaseStudyContainer>
     )
 };
 
