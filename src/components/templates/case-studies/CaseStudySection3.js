565c95bc57f42716ddc724c4a461d7f75874b3d3
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-11-06 17:29:42 +0530

    Add first half of case study template 3

---
+++ src/components/templates/case-studies/CaseStudySection3.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+import Video from "../../molecules/Video";
+
+import ImageSet from "../../molecules/ImageSet";
+
+import { HeaderText4 } from "../../atoms/HeaderText";
+import { BodyText3 } from "../../atoms/BodyText";
+import Image from "../../atoms/Image";
+
+const CaseStudySection3Container = styled.section`
+  .subsection-1 {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    grid-gap: ${props => props.theme.spacings[4]};
+    margin-bottom: ${props => props.theme.spacings[2]};
+  }
+
+  .description {
+  margin-top: ${props => props.theme.spacings[3]};
+  }
+
+  .image {
+    display: inline-block;
+  }
+
+  margin-bottom: ${props => props.theme.spacings[1]};
+`;
+
+const CaseStudySection3 = props => {
+  return (
+    <CaseStudySection3Container>
+      <div className="subsection-1">
+        <div>
+          <HeaderText4 >{props.heading[0].text}</HeaderText4>
+          <BodyText3 className="description" richText={props.description[0].text} />
+        </div>
+        <Image className="image" url={props.otherBlocks[0].image} />
+      </div>
+    </CaseStudySection3Container>
+  );
+};
+
+export default CaseStudySection3;
--- src/templates/case-study.js
+++ src/templates/case-study.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CaseStudyHeader from "../components/templates/case-studies/CaseStudyHeader";
 import CaseStudySection1 from "../components/templates/case-studies/CaseStudySection1";
 import CaseStudySection2 from "../components/templates/case-studies/CaseStudySection2";
+import CaseStudySection3 from "../components/templates/case-studies/CaseStudySection3";
 
 const CaseStudy = ({pageContext: {data}}) => {
     return (
@@ -9,6 +10,7 @@ const CaseStudy = ({pageContext: {data}}) => {
             <CaseStudyHeader {...data.header[0]}/>
             <CaseStudySection1 {...data.sections[0]}/>
             <CaseStudySection2 {...data.sections[1]}/>
+            <CaseStudySection3 {...data.sections[2]}/>
         </div>
     )
 };
