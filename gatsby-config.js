f20b381e95bc834b271b2bf2f3d3173b49515cfd
Meet Bhatt <meetbhatt@obvious.in>
2019-11-07 21:43:46 +0530

    Populate collection field on articles

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
                 homeSlug: 'home',
                 version: 'published',
                 includeLinks: true,
-                resolveRelations: ['case-study.relatedArticles']
+                resolveRelations: ['article.collection', 'case-study.relatedArticles']
             }
         },
         `gatsby-plugin-offline`
