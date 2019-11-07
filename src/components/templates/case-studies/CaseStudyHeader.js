5f995d0424143eb562ee7f40bd60b967c6f72625
Meet Bhatt <meetbhatt@obvious.in>
2019-11-07 22:29:00 +0530

    Fix case study header to use page header component

--- src/components/templates/case-studies/CaseStudyHeader.js
+++ src/components/templates/case-studies/CaseStudyHeader.js
@@ -6,29 +6,25 @@ import {BodyText2} from "../../atoms/BodyText";
 import LinkGroup from "../../molecules/LinkGroup";
 
 const CaseStudyHeaderContainer = styled.section`
-  max-width: 60%;
-  padding-top: ${props => props.theme.spacings[1]};
   .caption {
     color: ${props => props.theme.colors.lighterBlack};
     margin-bottom: ${props => props.theme.spacings[3]};
-  }
-  .title {
-    margin-bottom: ${props => props.theme.spacings[4]};
   }
   .links {
     color: ${props => props.theme.colors.blue};
     font-size: ${props=> props.theme.fontSizes[5]};
     line-height: ${props=> props.theme.lineHeights[2]};
   }
-  margin-bottom: ${props => props.theme.spacings[1]};
 `;
 
 const CaseStudyHeader = (props) => {
     return (
         <CaseStudyHeaderContainer>
-            <PageHeader className='title' text={props.title[0].text}/>
-            <BodyText2 className='caption' richText={props.caption[0].text}/>
-            <LinkGroup className='links' links={props.links[0].links}></LinkGroup>
+            <PageHeader className='title' title={props.title[0].text}>
+                <BodyText2 className='caption' richText={props.caption[0].text}/>
+                <LinkGroup className='links' links={props.links[0].links}></LinkGroup>
+            </PageHeader>
+
         </CaseStudyHeaderContainer>
     )
 };
