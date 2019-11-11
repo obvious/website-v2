83f3b30a271e8437164b4b8fb16f242544800be8
Meet Bhatt <meetbhatt@obvious.in>
2019-11-11 12:54:46 +0530

    Refactor page header component to also include caption

--- src/components/templates/case-studies/CaseStudyHeader.js
+++ src/components/templates/case-studies/CaseStudyHeader.js
@@ -2,27 +2,22 @@ import React from 'react'
 import styled from "styled-components";
 
 import PageHeader from "../common/PageHeader";
-import {BodyText2} from "../../atoms/BodyText";
 import LinkGroup from "../../molecules/LinkGroup";
 
 const CaseStudyHeaderContainer = styled.section`
-  .caption {
-    color: ${props => props.theme.colors.lighterBlack};
-    margin-bottom: ${props => props.theme.spacings[3]};
-  }
   .links {
     color: ${props => props.theme.colors.blue};
     font-size: ${props=> props.theme.fontSizes[5]};
     line-height: ${props=> props.theme.lineHeights[2]};
+    margin-top: ${props => props.theme.spacings[3]};
   }
 `;
 
 const CaseStudyHeader = (props) => {
     return (
         <CaseStudyHeaderContainer>
-            <PageHeader className='title' title={props.title[0].text}>
-                <BodyText2 className='caption' richText={props.caption[0].text}/>
-                <LinkGroup className='links' links={props.links[0].links}></LinkGroup>
+            <PageHeader className='title' title={props.title[0].text} caption={props.caption[0].text}>
+                <LinkGroup className='links' links={props.links[0].links}/>
             </PageHeader>
 
         </CaseStudyHeaderContainer>
--- src/components/templates/common/PageHeader.js
+++ src/components/templates/common/PageHeader.js
@@ -2,20 +2,26 @@ import React from 'react'
 import styled from "styled-components";
 
 import {HeaderText1} from "../../atoms/HeaderText";
+import {BodyText2} from "../../atoms/BodyText";
 
 const PageHeaderContainer = styled.div`
   max-width: 60%;
   padding-top: ${props => props.theme.spacings[1]};
+  margin-bottom: ${props => props.theme.spacings[1]};
   .title {
     margin-bottom: ${props => props.theme.spacings[4]};
   }
-  margin-bottom: ${props => props.theme.spacings[1]};
+  .caption {
+    color: ${props => props.theme.colors.lighterBlack};
+    margin-bottom: ${props => props.theme.spacings[3]};
+  }
 `;
 
 const PageHeader = (props) => {
     return (
         <PageHeaderContainer>
             <HeaderText1 className='title'>{props.title}</HeaderText1>
+            {props.caption && <BodyText2 className="caption" richText={props.caption}/>}
             {props.children}
         </PageHeaderContainer>
     )
