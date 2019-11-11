f2af515fa75fa7f7095a8e7808aebf3a73d350ed
Meet Bhatt <meetbhatt@obvious.in>
2019-11-11 12:55:25 +0530

    Add basic templates to single-article and single-article-collection pages

--- src/templates/article-collection.js
+++ src/templates/article-collection.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import styled from "styled-components";
 
-const ArticleCollection = ({pageContext}) => {
+import PageHeader from "../components/templates/common/PageHeader";
+import Image from "../components/atoms/Image";
+import {BodyText3, BodyText2} from "../components/atoms/BodyText";
+
+const ArticleCollectionContainer = styled.main`
+  .page-content {
+    display: grid;
+    grid-template-columns: 4fr 8fr;
+    grid-gap: ${props => props.theme.spacings[3]};
+    padding-bottom: ${props => props.theme.spacings[0]};
+  }
+  .caption {
+    color: ${props => props.theme.colors.lighterBlack};
+  }
+`;
+
+const ArticleCollection = ({pageContext, theme}) => {
 
     return (
-        <div>
-            {JSON.stringify(pageContext.data)}
-        </div>
+        <ArticleCollectionContainer theme={theme}>
+            <PageHeader title={pageContext.data.name} caption="This is a collection of articles"/>
+            <div className="page-content">
+                <Image className="featured-image" url={pageContext.data.content.featuredImage}/>
+                <BodyText3 className="text" richText={pageContext.data.content.caption} />
+            </div>
+        </ArticleCollectionContainer>
     )
 };
 
--- src/templates/article.js
+++ src/templates/article.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import styled from "styled-components";
 
-const Article = ({pageContext}) => {
+import PageHeader from "../components/templates/common/PageHeader";
+import Image from "../components/atoms/Image";
+import {BodyText3} from "../components/atoms/BodyText";
+
+const ArticleContainer = styled.main`
+  .page-content {
+    display: grid;
+    grid-template-columns: 4fr 8fr;
+    grid-gap: ${props => props.theme.spacings[3]};
+    padding-bottom: ${props => props.theme.spacings[0]};
+  }
+`;
+
+const Article = ({pageContext, theme}) => {
 
     return (
-        <div>
-            {JSON.stringify(pageContext.data)}
-        </div>
+        <ArticleContainer theme={theme}>
+            <PageHeader title={pageContext.data.name} caption="This is a single article"/>
+            <div className="page-content">
+                <Image className="featured-image" url={pageContext.data.content.featuredImage}/>
+                <BodyText3 className="text" richText={pageContext.data.content.caption} />
+            </div>
+        </ArticleContainer>
     )
 };
 
