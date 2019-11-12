5d005b6b1f1dadedadd8d5e76d4d474f0fdde614
Meet Bhatt <meetbhatt@obvious.in>
2019-11-12 11:45:29 +0530

    Add jsdoc comments to utility functions

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,19 @@
+/**
+ * A function to sanitise the response objects of queries of all kinds from the storyblok-source-gatsby plugin
+ * @description The function primarily looks for the JSON-stringified `content` key in the response and turns it into an object
+ * @param response - The response object from any kind of graphql query from the storyblok-source-gatsby plugin
+ * @returns {Object} - Sanitised object with no stringified values
+ */
 const prepareStoryblokGraphqlResponse = (response) => {
-    let data;
+    /** Declare variable to store the value that will be returned, defaults to null */
+    let data = null;
+
+    /** Check if the query is of the type `storyblokEntry` */
     if('storyblokEntry' in response) {
         data = JSON.parse(response.storyblokEntry.content);
     }
+
+    /** Check if the query is of the type `allStoryblokEntry` */
     else if('allStoryblokEntry' in response) {
         data = response.allStoryblokEntry.edges.map(edge => {
             let node = edge.node;
@@ -16,19 +27,20 @@ const prepareStoryblokGraphqlResponse = (response) => {
 
         });
     }
-    else {
-        data = {content: `{}` }
-    }
-    if(response) {
-        return data;
 
-    }
+    return data;
 };
 
-const groupBy = (list, keyGetter) => {
+/**
+ * A function to group array of objects based on the criteria provided
+ * @param list - Array of objects to be grouped by
+ * @param criteriaGetter - Function that returns the criteria for the grouping
+ * @returns {Object} - Object with criteria keys, with value equal to the corresponding array of objects
+ */
+const groupBy = (list, criteriaGetter) => {
     const map = new Map();
     list.forEach((item) => {
-        const key = keyGetter(item);
+        const key = criteriaGetter(item);
         const collection = map.get(key);
         if (!collection) {
             map.set(key, [item]);
@@ -36,6 +48,8 @@ const groupBy = (list, keyGetter) => {
             collection.push(item);
         }
     });
+
+    /** Convert the generated map into an object, which will be returned by the function */
     let groupedBy = {};
     for(let [key,val] of map.entries()){
         groupedBy[key]= val;
@@ -43,18 +57,37 @@ const groupBy = (list, keyGetter) => {
     return groupedBy;
 };
 
+
+/**
+ * A function to turn an array of articles (with collection object populated in each) into an array of collections each containing metadata and an array of articles
+ * @param preparedStoryblokResponse - Sanitised response object from prepareStoryblokGraphqlResponse
+ * @see prepareStoryblokGraphqlResponse
+ * @returns {any[]} - An array of collections each containing metadata and an array of articles
+ */
 const prepareArticlesFromMultipleCollections = (preparedStoryblokResponse) => {
+    /** First, group the articles by collection uuids */
     let articleCollectionsDataGroupedByCollection = groupBy(preparedStoryblokResponse, article => article.content.collection.uuid);
 
+    /** Convert the grouped object to have collection metadata as well */
     return Object.keys(articleCollectionsDataGroupedByCollection).map(key => {
         const articleGroup = articleCollectionsDataGroupedByCollection[key];
+
+        /** Clone the collection object to avoid overwriting the original object */
         let collectionData = Object.assign({}, articleGroup[0].content.collection);
+
         collectionData.articles = articleGroup;
         return collectionData;
     });
 };
 
-const formatDate = (date, options) => {
+/**
+ * A function to return formatted date string from UTC strings
+ * @param utcDateString - A date string in UTC format
+ * @param options - An object of options to be passed on to toLocaleDateString()
+ * @returns {string} - Formatted date string
+ */
+const formatDate = (utcDateString, options) => {
+    /** Declare default options for when no options are provided */
     const defaultOptions = {
         month: 'short',
         day: '2-digit'
