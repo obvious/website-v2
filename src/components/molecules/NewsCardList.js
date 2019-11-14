95bc29fe0d6ad00dd80f879884e84c309ffc1794
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-11-14 18:35:55 +0530

    Reformat data to be able to pull featured image

--- src/components/molecules/NewsCardList.js
+++ src/components/molecules/NewsCardList.js
@@ -12,21 +12,29 @@ const NewsCardListContainer = styled.ol`
 export const NewsCardList = props => {
   let newsStories = Object.values(props);
   newsStories = newsStories.map(newsStory => {
-      const content = JSON.parse(newsStory.content);
+    const content = JSON.parse(newsStory.content);
     return {
-        name: newsStory.name,
-        date_published: new Date(content.date_published).toLocaleDateString(undefined, {month: "short", day:"2-digit"}),
-        context_setting_text: content.context_setting_text,
-        feature_image: content.related_story[0].article && content.related_story[0].article.content.featuredImage.length ? content.related_story[0].article.content.featuredImage : null
+      name: newsStory.name,
+      date_published: new Date(
+        content.date_published
+      ).toLocaleDateString(undefined, { month: "short", day: "2-digit" }),
+      context_setting_text: content.context_setting_text,
+      feature_image:
+        content.related_story[0].article &&
+        content.related_story[0].article.content.featuredImage.length
+          ? content.related_story[0].article.content.featuredImage
+          : null
     };
   });
 
   return (
-    <NewsCardListContainer>
+    <NewsCardListContainer {...props}>
       <HeaderText2>News</HeaderText2>
-      {newsStories.map(newsStory => (
-        <NewsCard {...newsStory} />
-      ))}
+      <div className="newsCard-group">
+        {newsStories.map(newsStory => (
+          <NewsCard {...newsStory} />
+        ))}
+      </div>
     </NewsCardListContainer>
   );
 };
