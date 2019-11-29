500d9b0401d81b16d91d90d490e5242cad01d4d0
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-11-29 17:30:46 +0530

    Move GraphPoint into molecule

--- src/components/molecules/GraphPoint.js
+++ src/components/molecules/GraphPoint.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { CaptionText2, CaptionTextHighlighted } from "./CaptionText";
-import { Divider } from "./Divider";
+import { CaptionText2, CaptionTextHighlighted } from "../atoms/CaptionText";
+import { Divider } from "../atoms/Divider";
 
 const GraphPointContainer = styled.div`
   display: inline-block;
