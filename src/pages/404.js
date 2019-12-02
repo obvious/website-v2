3aef28bbb5f24ad4e3da4b0b5e9662a256790f4e
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-12-02 10:23:44 +0530

    Create 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ComponentNotFound from "../components/component-not-found";
+
+export const PageNotFound = () => <ComponentNotFound />;
+
+export default PageNotFound;
