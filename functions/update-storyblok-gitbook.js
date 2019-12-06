75576ae3816f15843d923acd43c2657a717cdab7
Tanvi Bhakta <tanvibhakta@gmail.com>
2019-12-06 12:08:30 +0530

    Add bridges to read and write to Storyblok

--- functions/update-storyblok-gitbook.js
+++ functions/update-storyblok-gitbook.js
@@ -1,9 +1,41 @@
+const StoryblokClient = require("storyblok-js-client");
+const {
+  STORYBLOCK_MANAGEMENT_API_OAUTH_TOKEN,
+  STORYBLOCK_DELIVERY_API_ACCESS_TOKEN_DRAFT
+} = process.env;
+
 exports.handler = async (event, context) => {
-  if (event.httpMethod !== "POST") {
-    return {
-      statusCode: 405,
-      body: "Method not allowed"
-    };
+  // Uncomment once gitbook webhook is set up
+  // if (event.httpMethod !== "POST") {
+  //   return {
+  //     statusCode: 405,
+  //     body: "Hit me with a POST please"
+  //   };
+  // }
+
+  const SBread = new StoryblokClient({
+    accessToken: STORYBLOCK_DELIVERY_API_ACCESS_TOKEN_DRAFT,
+    cache: {
+      clear: "auto",
+      type: "memory"
+    }
+  });
+
+  const SBwrite = new StoryblokClient({
+    oauthToken: STORYBLOCK_MANAGEMENT_API_OAUTH_TOKEN
+  });
+
+  const gitbookStories = SBread.get("cdn/stories", {
+    starts_with: "gitbook-articles/",
+    version: "draft"
+  });
+
+  // Get modified gitbook articles and match it to the respective slugs here
+
+  if (gitbookStories.length > 0) {
+    // gitbookStories.forEach(story => {
+    // Storyblok.put(story.full_slug)
+    // })
   }
 
   return {
